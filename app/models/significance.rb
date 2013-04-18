304b6933cf4240cfe86c5231be873ea999d12920
Carles Andrés <carlos.andres.c@gmail.com>
2013-04-18 14:57:40 +0200

    Removed Significance model as I found it is not really necessary

--- app/models/significance.rb
+++
@@ -1,3 +0,0 @@
-class Significance < ActiveRecord::Base
-  attr_accessible :meaning_id, :word_id
-end
--- spec/factories/significances.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :significance do
-    word_id 1
-    meaning_id 1
-  end
-end
