d6bf7baba52c0c98f728d8f65717a69be3e355fb
Carles Andrés <carlos.andres.c@gmail.com>
2013-04-26 23:31:24 +0200

    Initial migration ported from initial schema.rb as per Rails Recipes indications

--- db/migrate/20130416154903_create_meanings.rb
+++
@@ -1,8 +0,0 @@
-class CreateMeanings < ActiveRecord::Migration
-  def change
-    create_table :meanings do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130418134447_create_meaning_word_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateMeaningWordJoinTable < ActiveRecord::Migration
-  def change
-    create_table :meanings_words, :id => false do |t|
-      t.integer :meaning_id
-      t.integer :word_id
-    end
-  end
-end
