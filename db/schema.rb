9e0bbb1aa0eab938c2796d693d5d5503ab0412bd
Carles Andrés <carlos.andres.c@gmail.com>
2013-04-26 23:11:31 +0200

    Schema for legacy MySQL database as-is

--- db/schema.rb
+++ db/schema.rb
@@ -13,55 +13,118 @@
 
 ActiveRecord::Schema.define(:version => 0) do
 
+  create_table "antonyms", :force => true do |t|
+    t.integer "word_meaning_id1", :default => 0, :null => false
+    t.integer "word_meaning_id2", :default => 0, :null => false
+  end
+
+  add_index "antonyms", ["word_meaning_id1", "word_meaning_id2"], :name => "word_meaning_id1"
+  add_index "antonyms", ["word_meaning_id1"], :name => "word_meaning_id1_2", :unique => true
+  add_index "antonyms", ["word_meaning_id2"], :name => "word_meaning_id2", :unique => true
+
+  create_table "db_sequence", :primary_key => "seq_name", :force => true do |t|
+    t.integer "nextid", :default => 0, :null => false
+  end
+
   create_table "meanings", :force => true do |t|
-    t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.string    "distinction"
+    t.integer   "subject_id"
+    t.integer   "hidden",           :limit => 1, :default => 0
+    t.integer   "check_count",                   :default => 0, :null => false
+    t.integer   "super_id"
+    t.integer   "morphologic_id"
+    t.integer   "morphologic_1_id"
+    t.integer   "morphologic_2_id"
+    t.timestamp "when_added",                                   :null => false
   end
 
   create_table "rejection_causes", :force => true do |t|
     t.string "cause_short_description", :limit => 40, :null => false
   end
 
-  create_table "searches", :force => true do |t|
-    t.integer  "word_id",    :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at"
+  create_table "removed_words", :force => true do |t|
+    t.string "word", :limit => 256, :null => false
   end
 
-  add_index "searches", ["word_id"], :name => "word_id"
-
-  create_table "significations", :id => false, :force => true do |t|
-    t.integer  "word_id",    :default => 0, :null => false
-    t.integer  "meaning_id", :default => 0, :null => false
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+  create_table "search_history", :force => true do |t|
+    t.integer   "word_id",      :null => false
+    t.timestamp "timeofsearch", :null => false
   end
 
-  create_table "user-suggestions", :force => true do |t|
-    t.string   "word",               :limit => 256, :null => false
-    t.integer  "status",             :limit => 2,   :null => false
-    t.datetime "created_at",                        :null => false
-    t.integer  "rejection_cause_id",                :null => false
-    t.integer  "user_meant_word_id",                :null => false
-    t.datetime "updated_at",                        :null => false
+  add_index "search_history", ["word_id"], :name => "word_id"
+
+  create_table "subjects", :force => true do |t|
+    t.string "subject",     :limit => 50, :default => "", :null => false
+    t.string "explanation"
+  end
+
+  add_index "subjects", ["subject"], :name => "subject"
+
+  create_table "suggested", :force => true do |t|
+    t.string    "word",               :limit => 256, :null => false
+    t.integer   "status",             :limit => 2,   :null => false
+    t.timestamp "when_suggested",                    :null => false
+    t.integer   "rejection_cause_id"
+    t.integer   "user_meant_word_id"
   end
 
   create_table "user_comments", :force => true do |t|
-    t.datetime "created_at",                     :null => false
-    t.string   "email",           :limit => 70,  :null => false
-    t.integer  "word_id",                        :null => false
-    t.text     "comments",        :limit => 255, :null => false
-    t.integer  "revision_status"
+    t.timestamp "timestamp",                      :null => false
+    t.string    "email",           :limit => 70,  :null => false
+    t.integer   "word_id",                        :null => false
+    t.text      "comments",        :limit => 255, :null => false
+    t.integer   "revision_status"
   end
+
+  create_table "uses", :force => true do |t|
+    t.string "name",      :default => "", :null => false
+    t.string "shortname", :default => "", :null => false
+  end
+
+  add_index "uses", ["name"], :name => "name", :unique => true
+
+  create_table "usuarios", :force => true do |t|
+    t.string "username", :limit => 256, :null => false
+    t.string "password", :limit => 40,  :null => false
+  end
+
+  create_table "wiktionary", :id => false, :force => true do |t|
+    t.string "headword", :default => "", :null => false
+    t.text   "meanings"
+    t.text   "synonyms"
+  end
+
+  add_index "wiktionary", ["headword"], :name => "headword"
+
+  create_table "word_forms", :force => true do |t|
+    t.string "word", :limit => 100, :default => "", :null => false
+  end
+
+  add_index "word_forms", ["word"], :name => "word"
+
+  create_table "word_mapping", :id => false, :force => true do |t|
+    t.integer "derived_id", :default => 0, :null => false
+    t.integer "base_id",    :default => 0, :null => false
+  end
+
+  add_index "word_mapping", ["derived_id"], :name => "derived_id"
+
+  create_table "word_meanings", :force => true do |t|
+    t.integer   "word_id",    :default => 0, :null => false
+    t.integer   "meaning_id", :default => 0, :null => false
+    t.integer   "use_id"
+    t.timestamp "when_added",                :null => false
+  end
+
+  add_index "word_meanings", ["meaning_id"], :name => "meaning_id"
+  add_index "word_meanings", ["word_id"], :name => "word_id"
 
   create_table "words", :force => true do |t|
-    t.string   "entry"
-    t.string   "lookup"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string    "word",      :default => "", :null => false
+    t.string    "lookup"
+    t.timestamp "was_added",                 :null => false
   end
 
-  add_index "words", ["entry"], :name => "word", :unique => true
+  add_index "words", ["word"], :name => "word", :unique => true
 
 end
