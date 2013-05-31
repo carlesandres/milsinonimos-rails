4b6e5caf8f50f6f19280ef707c278781882f2e3c
Carles Andrés <carlos.andres.c@gmail.com>
2013-05-31 02:56:11 +0200

    Implement routing/navigation for words in the results box

--- bbone/app/scripts/routes/Router.js
+++ bbone/app/scripts/routes/Router.js
@@ -5,27 +5,35 @@ define([
     'backbone',
     'views/App-view',
     'collections/Meaning-collection',
-], function ($, Backbone, AppView, MeaningsCollection) {
+], function ($, Backbone, AppView, word, MeaningsCollection) {
     'use strict';
 
     var AppRouter = Backbone.Router.extend({
         routes: {
-            'aja':               'log',
-            '':                  'empty'
+            ':word': 'updateWord'
         },
 
         initialize: function () {
+            var route = this;
             Backbone.history.start( { pushState: true, hashChange: false } );
+            $(document).on('click', 'a:not([data-bypass])', function (evt) {
+
+                var href = $(this).attr('href');
+                var protocol = this.protocol + '//';
+
+                if (href.slice(protocol.length) !== protocol) {
+                    evt.preventDefault();
+                    route.navigate(href, true);
+                }
+            });
+
             this.view = new AppView();
             this.collection = new MeaningsCollection( );
         },
 
-        empty: function() {
-            console.log('empty');
-        },
-
-        log: function() {
-            console.log('aja');
+        updateWord: function( searchedterm ) {
+            $('#searchbox').val( searchedterm );
+            this.view.trySearch();
         },
     });
 
