f0c01972db49c838cc31a6861c0c3a651b50a10e
Carles Andrés <carlos.andres.c@gmail.com>
2013-06-05 17:50:53 +0200

    First version on Rails rendered synonim search

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,13 +1,7 @@
 class MainController < ApplicationController
-  respond_to :html
 
   def show
-    @meanings = Word.includes(:meanings => :synonims).find_by_entry(params[:name])
-
-    respond_with(@meanings)
-#    respond_with(@meanings) do |format|
-#      format.json { render json: @meanings.as_json(only: [:entry], include: {meanings: {only: [:id], include: {synonims: {only: [:entry]}}}}) }
-#    end
+    @word = Word.includes(:meanings => :synonims).find_by_entry(params[:name])
 
   end
 end
