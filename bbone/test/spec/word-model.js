00876938bb1f73538c4ee4fb95fba70747226e6a
Carles Andrés <carlos.andres.c@gmail.com>
2013-06-19 19:22:33 +0200

    First scaffold for testing with mocha

---
+++ bbone/test/spec/word-model.js
@@ -0,0 +1,19 @@
+/*global describe, it */
+'use strict';
+(function () {
+    describe('Give it some context', function () {
+        describe('maybe a bit more context here', function () {
+            it('should run here few assertions', function () {
+              //var a = new Word
+
+
+            });
+        });
+    });
+
+    describe('Querying /words/palabra', function () {
+        it('should return an 200 response', function () {
+
+        });
+    });
+})();
