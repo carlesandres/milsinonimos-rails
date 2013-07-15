6e73a3262587cacb002ac3e7a9142c026c91ee38
Carles Andrés <carlos.andres.c@gmail.com>
2013-07-16 01:11:32 +0200

    First real tests

--- bbone/test/spec/SpecRunner.js
+++ bbone/test/spec/SpecRunner.js
@@ -24,16 +24,23 @@ require.config({
         underscore: '../app/components/underscore-amd/underscore',
         bootstrap: '../app/vendor/bootstrap',
         templates: '../app/templates',
-        models: '../app/scripts/models'
+        models: '../app/scripts/models',
+        collections: '../app/scripts/collections'
     }
 });
 
 require([ 'backbone', 'models/word-model' ], function (Backbone, wordmodel) {
 
-        it('should be a test', function( ) {
-            describe('that exists and fails', function () {
+        it('Word model', function( ) {
+            describe('should exist', function () {
                 expect( wordmodel ).to.exist ;
             });
+            describe('should have an initialize method', function () {
+                expect( wordmodel.initialize ).to.be.defined;
+            });
+            describe('it fires the custom update event when id changes', function () {
+            });
+
         });
 
         mocha.run();
